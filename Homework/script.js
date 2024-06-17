ffdcfe78dfd67641abf6b68d00616d78fea6e0f0
Vitaliy <kv1993@me.com>
2024-06-17 18:18:18 +0800

    Updated homework added products from json

---
+++ Homework/script.js
@@ -0,0 +1,37 @@
+const dataUrl = "data.json";
+
+async function fetchData(url) {
+    try {
+        const responce = await fetch(url);
+        console.log(responce);
+        const data = await responce.json();
+        return data;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const data = await fetchData(dataUrl);
+
+    const fiBlockEl = document.querySelector('.featured-items .fi-items');
+
+    data.forEach(el => {
+
+        fiBlockEl.insertAdjacentHTML("beforeend", `
+        <div class="fi-item">
+            <div class="fi-photo">
+                <img src="${el.img}" alt="${el.name}">
+                <div class="hover">
+                    <a href="#">Add to Cart</a>
+                </div>
+            </div>
+            <div class="desc">
+                <h4>${el.name}</h4>
+                <div class="price">${el.price}</div>
+            </div>
+        </div>
+        `)
+
+        });
+});
